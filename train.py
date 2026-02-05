945702e149da32f8f1d2439b62aaade36dbcb9e1
zhelianl-zl <zhelianl@andrew.cmu.edu>
2026-02-05 00:02:21 -0500

    change the config

--- train.py
+++ train.py
@@ -4540,8 +4540,8 @@ def set_seed(seed: int):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = False
-    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 def init_wandb_if_needed(args, default_name: str):
     import os, wandb
