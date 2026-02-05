8322cd75f4b3590c81b973d5336db350d9586781
zhelianl-zl <zhelianl@andrew.cmu.edu>
2026-02-05 17:06:18 -0500

    change project name

--- train.py
+++ train.py
@@ -4497,7 +4497,7 @@ if __name__ == '__main__':
     print(f"[CFG] LOSS2 wrong={LOSS_2nd_stage_wrong}, correct={LOSS_2nd_stage_correct}, option={option_stage2}")
 
 def init_wandb_if_needed(args, default_name: str):
-    project = "ecg_SVHN" #os.environ.get("WANDB_PROJECT", "ecg_binary_fast")
+    project = "ecg_Cifar_100" #os.environ.get("WANDB_PROJECT", "ecg_binary_fast")
     entity  = os.environ.get("WANDB_ENTITY", None)
 
     name = os.environ.get("WANDB_NAME", None) or default_name
@@ -4545,7 +4545,7 @@ def set_seed(seed: int):
 
 def init_wandb_if_needed(args, default_name: str):
     import os, wandb
-    project = "ecg_SVHN" #os.environ.get("WANDB_PROJECT", "ecg_binary_fast")
+    project = "ecg_Cifar_100" #os.environ.get("WANDB_PROJECT", "ecg_binary_fast")
     entity  = os.environ.get("WANDB_ENTITY", None)
     name    = os.environ.get("WANDB_NAME", None) or default_name
     group   = os.environ.get("WANDB_GROUP", None) or f"{args.dataset}_seed{args.seed}_T{args.stop_val}"
