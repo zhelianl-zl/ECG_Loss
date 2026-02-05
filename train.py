28bc770c729fa2b45f32596eb3196167745d91aa
zhelianl-zl <zhelianl@andrew.cmu.edu>
2026-02-04 22:28:10 -0500

    turn on benchmark

--- train.py
+++ train.py
@@ -4491,8 +4491,8 @@ def set_seed(seed: int):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = False
+    torch.backends.cudnn.benchmark = True
 
 def init_wandb_if_needed(args, default_name: str):
     import os, wandb
